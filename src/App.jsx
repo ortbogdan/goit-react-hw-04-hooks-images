3a09ce25bc663f84105e19db8f021f09769127ad
Bogdan <ortbg1988@gmail.com>
2022-01-20 23:22:52 +0200

    [refactor] Refactoring all file and code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,23 @@
 
 import React, { Component } from "react";
-import axios from "axios";
+import { Container } from "./App.styled";
 import {Searchbar, ImageGallery} from './components/index'
-import "../src/styles.css";
 
-const API_KEY = '24332331-fceed411956b076254def86c5';
-axios.defaults.baseURL = 'https://pixabay.com';
 export class App extends Component {
   state = {
-    images: [],
-    filter: 'cat',
-    page: 1,
-    loading: false,
+    filter: '',
 }
-async componentDidUpdate(_, prevState) {
-    if (prevState.filter !== this.state.filter) {
-      const response = await axios.get(`/api/?q=${this.state.filter}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
-      this.setState({images: response.data.hits})
-    }
-  
-}
-  
-  
-  async componentDidMount() {
-    this.setState({loading: true})
-    try {
-      const response = await axios.get(`/api/?q=${this.state.filter}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
-      this.setState({images: response.data.hits})
-      console.log(response.data.hits)
-    } catch (error) {
-      console.log(error)
-    } finally {
-      this.setState({loading: false})
-    }
-  
-  }
-  handleFormSabmit = (searchedImg) => {
-    this.setState({ filter: searchedImg });
+
+  handleFormSabmit = (filter) => {
+    this.setState({ filter });
   }
 
   render() {
     return (
-      <>
+      <Container>
         <Searchbar onSubmitForm={this.handleFormSabmit} />
-        {this.state.loading && <h2>Download...</h2>}
-        <ImageGallery images={this.state.images}></ImageGallery>
-      </>);}
+        <ImageGallery filter={this.state.filter}></ImageGallery>
+      </Container>);}
  
 }
 
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -7,18 +7,21 @@ export class Searchbar extends Component  {
   }
   handleChange = (event) => {
     const searchedImg = event.currentTarget.value;
-    const normalizeSearcedImg = searchedImg.trim().toLowerCase()
-    this.setState({searchImg: normalizeSearcedImg })
+    const normalizeSearchedImg = searchedImg.trim().toLowerCase();
+    this.setState({ searchImg: normalizeSearchedImg });
   }
   handleSabmit = (event) => {
     event.preventDefault();
+    if (this.state.searchImg.trim() === '') {
+      return alert('Enter the name of the pictures!');
+    }
     this.props.onSubmitForm(this.state.searchImg);
     this.setState({searchImg: ''})
   }
   render() {
     return (<SearchbarWrapper className="searchbar">
       <SearchForm className="form" onSubmit={this.handleSabmit}>
-        <SearchFormButton type="submit" className="button" >
+        <SearchFormButton type="submit" className="button" aria-label="Search button" >
           <BiSearchAlt2 fill={ '#3f51b5' } size={36}/>
           {/* <SearchFormButtonLabel className="button-label"/> */}
         </SearchFormButton>
@@ -33,5 +36,4 @@ export class Searchbar extends Component  {
       </SearchForm>
     </SearchbarWrapper>)
   }
-    
 }
