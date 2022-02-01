266e93f12ba8f2cf74a4671bd78c865c61487e8f
Bogdan <ortbg1988@gmail.com>
2022-02-01 11:56:53 +0200

    [fix] Fix bug with deps in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 
-// import React, { Component } from "react";
-import { useState } from "react";
+
+import { useState, useEffect} from "react";
 import { Container } from "./App.styled";
 import { Searchbar, ImageGallery, Button, Loader, Modal } from './components/index'
 import { Api } from "./services/api";
 import * as Scroll from 'react-scroll';
-import { useEffect } from "react/cjs/react.development";
-
 
 export const App = () => {
   const [filter, setFilter] = useState('');
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
   const [totalImg, setTotalImg] = useState(0);
   const [error, setError] = useState(null);
-  const [showModal, setShowModal] = useState(false);
   const [imageUrl, setImageUrl] = useState('');
   
-  // useEffect(() => {
-  //   if (filter === '') {
-  //     return
-  //   }
-  // }, [])
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     if (filter === '') {
       return
@@ -34,18 +27,16 @@ export const App = () => {
     const { hits, total, totalHits } = await Api(filter, page);
       
     if (total) {
-      setImages([...images, ...hits]);
+      setImages((prevImages)=> [...prevImages, ...hits]);
       setTotalImg(totalHits);
-      
-      return 
+      return;
     }
     setError(new Error(`Oops...No pictures on your request - ${filter}`));
   } catch (error) {
-    this.setState({ error });
+    setError( error );
   } finally {
     setLoading(false)
-    setTimeout(()=>{Scroll.animateScroll.scrollToBottom();}, 500)
-    
+    setTimeout(()=>{Scroll.animateScroll.scrollToBottom();}, 700)
       }
     }
     fetchImages()
@@ -58,92 +49,26 @@ export const App = () => {
     setFilter(query);
     setPage(1);
     setTotalImg(0);
+    setImages([]);
   }
   const toggleModal = () => {
     setShowModal(!showModal);
   }  
-  
-  
-  
   const takeImageUrl = url => {
     setImageUrl(url)
     toggleModal()
   }
-  return (
-      <Container>
+  return <Container>
         <Searchbar onSubmitForm={handleFormSabmit} />
         {showModal && <Modal onClose={toggleModal}><img src={imageUrl} alt="" /></Modal>}
         {error && <p>{error.message}</p>}
         {images.length > 0 && <ImageGallery images={images} onModal={takeImageUrl}/>}
         {loading && <Loader/>}
         {totalImg !== images.length && <Button onClick={()=>setPage(page+1)}>Load more</Button>}
-      </Container>);
+      </Container>;
 }
 
 
 
-// export class App extends Component {
-//   state = {
-//     images: [],
-//     filter: '',
-//     page: 1,
-//     loading: false,
-//     totalHits: 0, 
-//     error: null, 
-//     showModal: false,
-//     imageUrl: ''
-//   }
-//   async componentDidUpdate(_, prevState) {
-//     const { filter } = this.state;
-//     if (prevState.filter !== filter) {
-//       this.fetchImages();
-//     }
-//   }
-// toggleModal = () => {
-//     this.setState(({showModal}) => ({showModal: !showModal}))
-// }  
-// handleFormSabmit = (filter) => {
-//     if (filter === this.state.filter) {
-//       return;
-//     }
-//     this.setState({ filter, page: 1, images: [], totalHits: 0 });
-//   }
-// fetchImages = async () => {
-//         const { images, page, filter } = this.state;
-//   try {
-//     this.setState({ loading: true, error: null })
-//     const { hits, total, totalHits } = await Api(filter, page);
-      
-//     if (total) {
-      
-//       return this.setState({ images: [...images, ...hits], totalHits: totalHits, page: page + 1 });
-//     }
-//     this.setState({ error: new Error(`Oops...No pictures on your request - ${filter}`) });
-//   } catch (error) {
-//     this.setState({ error });
-//   } finally {
-//     this.setState({ loading: false })
-//     setTimeout(()=>{Scroll.animateScroll.scrollToBottom();}, 500)
-    
-//   }
-// }
-//   takeImageUrl = url => {
-//     this.setState({ imageUrl: url })
-//     console.log(this.state.imageUrl)
-//     this.toggleModal()
-//   }
-//   render() {
-//     const { error, images, loading, totalHits, showModal, imageUrl } = this.state;
-//     return (
-//       <Container>
-//         <Searchbar onSubmitForm={this.handleFormSabmit} />
-//         {showModal && <Modal onClose={this.toggleModal}><img src={imageUrl} alt="" /></Modal>}
-//         {error && <p>{error.message}</p>}
-//         {images.length > 0 && <ImageGallery images={images} onModal={this.takeImageUrl}/>}
-//         {loading && <Loader/>}
-//         {totalHits !== images.length && <Button onClick={this.fetchImages}>Load more</Button>}
-//       </Container>);}
- 
-// }
 
 
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,43 +1,42 @@
-import React, { Component } from "react";
+
+import { useState } from "react";
 import { SearchbarWrapper, SearchForm, SearchFormButton, SearchFormInput } from "./Searchbar.styled";
 import { BiSearchAlt2 } from "react-icons/bi"
 import { ToastContainer, toast } from 'react-toastify';
 import PropTypes from "prop-types";
 import 'react-toastify/dist/ReactToastify.css';
-export class Searchbar extends Component  {
-  state = {
-    searchImg: ''
+
+export const Searchbar = ({ onSubmitForm }) => {
+const [searchImg, setSearchImg] = useState('')
+
+ const handleChange = (event) => {
+   const searchedImg = event.currentTarget.value.trim();
+   setSearchImg(searchedImg.toLowerCase());
   }
-  handleChange = (event) => {
-    const searchedImg = event.currentTarget.value.trim();
-    this.setState({ searchImg: searchedImg.toLowerCase() });
-  }
-  handleSabmit = (event) => {
+ const handleSabmit = event => {
     event.preventDefault();
-    if (this.state.searchImg.trim() === '') {
+    if (searchImg.trim() === '') {
       return toast('Enter a name of the pictures!');
     }
-    this.props.onSubmitForm(this.state.searchImg);
+    onSubmitForm(searchImg);
   }
-  render() {
-    return (<SearchbarWrapper className="searchbar">
-      <SearchForm className="form" onSubmit={this.handleSabmit}>
-        <SearchFormButton type="submit" className="button" aria-label="Search button" >
-          <BiSearchAlt2 fill={ '#3f51b5' } size={36}/>
-        </SearchFormButton>
-        <SearchFormInput
+  return <SearchbarWrapper className="searchbar">
+       <SearchForm className="form" onSubmit={handleSabmit}>
+         <SearchFormButton type="submit" className="button" aria-label="Search button" >
+           <BiSearchAlt2 fill={ '#3f51b5' } size={36}/>
+         </SearchFormButton>
+         <SearchFormInput
           className="input"
           type="text"
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
-          onChange={this.handleChange}
+          onChange={handleChange}
         />
       </SearchForm>
       <ToastContainer limit={1} autoClose={3000}/>
-    </SearchbarWrapper>)
+    </SearchbarWrapper>
   }
-}
 Searchbar.propTypes = {
   onSubmitForm: PropTypes.func.isRequired
 }
